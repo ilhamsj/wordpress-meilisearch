b6dcd2a74a9fa0048e6db327b50151e6987e81b4
ilhamsj <ilhamsaputrajati@gmail.com>
2025-05-30 17:04:59 +0700

    Remove handle_transition_post_status method from Post class to streamline post status handling and improve code clarity. Update handle_save_post method to maintain logging functionality for published posts.

--- hooks/Post.php
+++ hooks/Post.php
@@ -24,40 +24,12 @@ class Post {
         add_action('wp_trash_post', [$this, 'handle_delete_post'], 10, 3);
     }
 
-    public function handle_transition_post_status($new_status, $old_status, $post) {
-
-        if ($post->post_type !== 'post') return;
-
-        $is_new_status_published = $new_status === 'publish';
-        $is_old_status_published = $old_status === 'publish';
-
-        $response = [
-            'action' => null,
-            'post' => $post,
-        ];
-
-        if ($is_new_status_published && !$is_old_status_published) {
-            $response['action'] = 'create';
-            $this->logger->info(__FUNCTION__, $response);
-        }
-
-        if ($is_new_status_published && $is_old_status_published) {
-            $response['action'] = 'update';
-            $this->logger->info(__FUNCTION__, $response);
-        }
-
-        if (!$is_new_status_published && $is_old_status_published) {
-            $response['action'] = 'delete';
-            $this->logger->info(__FUNCTION__, $response);
-        }
-    }
-
     public function handle_save_post(int $post_id, WP_Post $post, bool $update) {
         if (!in_array($post->post_type, Config::POST_TYPE)) return;
 
         $post_data = [];
         $post_data = $this->response->default($post_id);
-        
+
         if($update && $post->post_status === 'publish') {
             $this->logger->info(__FUNCTION__, $post_data);
             $this->apiCall->send('post', $post_data);
